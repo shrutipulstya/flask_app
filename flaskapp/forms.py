e0784720ac2dccc6f83cfd448ef08a710ea58f22
Shruti <shrutipulstya9@gmail.com>
2020-11-03 00:02:56 +0530

    updated user account and profile picture

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskapp.models import User
@@ -21,6 +23,25 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email= email.data).first()
         if user:
             raise ValidationError('email is taken please choose a diff one.')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators = [DataRequired(), Length(min=2, max=20)] )
+    email = StringField('Email', 
+                            validators = [DataRequired(), Email()] )
+    picture = FileField('Update Profile Picture', validators = [ FileAllowed(['jpg', 'jpeg', 'png'])] )
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username= username.data).first()
+            if user:
+                raise ValidationError('Username is taken please choose a diff one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email= email.data).first()
+            if user:
+                raise ValidationError('email is taken please choose a diff one.')
 
 
 class LoginForm(FlaskForm):
