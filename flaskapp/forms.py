0f5527c1d4f6419f7f2e8b9935b42b1d36258603
Shruti <shrutipulstya9@gmail.com>
2020-11-04 00:34:46 +0530

    added create,update and delete posts functionality

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskapp.models import User
 
@@ -49,3 +49,8 @@ class LoginForm(FlaskForm):
     password = PasswordField( 'Password',validators = [DataRequired()] )
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators = [DataRequired()])
+    content = TextAreaField('Content', validators = [DataRequired()])
+    submit = SubmitField('Post')
