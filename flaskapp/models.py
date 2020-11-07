cc6f4f4a4eb199c99421271860098ab337fba4cf
Shruti <shrutipulstya9@gmail.com>
2020-11-07 19:45:16 +0530

    re-structured the package using blueprints and configured flaskapp

--- flaskapp/models.py
+++ flaskapp/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flaskapp import db, login_manager, app
+from flask import current_app
+from flaskapp import db, login_manager
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -17,12 +18,12 @@ class User(db.Model, UserMixin):
 
 
     def get_reset_token(self, expires_sec = 1800):
-        s= Serializer(app.config['SECRET_KEY'], expires_sec)
+        s= Serializer(current_app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id':self.id}).decode('utf-8')
 
     @staticmethod
     def verify_reset_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
+        s = Serializer(current_app.config['SECRET_KEY'])
         try:
             user_id = s.loads(token)['user_id']
         except:
--- flaskapp/users/forms.py
+++ flaskapp/users/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import current_user
 from flaskapp.models import User
 
 class RegistrationForm(FlaskForm):
@@ -49,11 +49,6 @@ class LoginForm(FlaskForm):
     password = PasswordField( 'Password',validators = [DataRequired()] )
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-
-class PostForm(FlaskForm):
-    title = StringField('Title', validators = [DataRequired()])
-    content = TextAreaField('Content', validators = [DataRequired()])
-    submit = SubmitField('Post')
 
 class RequestResetForm(FlaskForm):
     email = StringField('Email', validators = [DataRequired(), Email()] )
